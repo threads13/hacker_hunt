0368d30b688c7742fd86be065359011bfb9ddc64
Jacob Hoke <jacob@threads13.com>
2018-02-07 22:48:43 -0500

    began to set up stat on search component

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,6 @@ class App extends Component {
         <Topics />
         <Subscribe />
         <List/>
-
       </div>
     );
   }
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+  }
 
   render() {
     return (
       <div
         style={{textAlign: 'center', marginTop: '1em', marginBottom: '4em'}}
         className="searchbar">
-        <input></input>
+        <input
+          value={this.state.term}
+          onChange={event => this.handleInputChange(event.target.value)}
+        />
       </div>
     );
+  }
+
+  handleInputChange(term){
+    this.setState({ term });
+
   }
 
 }
