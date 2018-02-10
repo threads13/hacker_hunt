16a54d924dd87f32d4ff2619ac5f916db3327353
Jacob Hoke <jacob@threads13.com>
2018-02-10 03:51:25 -0500

    styled border and padding on list-item component

--- src/components/list_item.js
+++ src/components/list_item.js
@@ -4,9 +4,15 @@ const ListItem = props => {
   console.log(props.title);
   return (
     <div>
-      <li>{props.title}</li>
+      <li style={{marginTop: '20px'}}>{props.title}</li>
       <li>{props.desc}</li>
-      <li style={{marginBottom: '40px', borderBottom: 'solid 1px gray'}}><em>{props.tags}</em></li>
+      <li style={{
+          marginBottom: '10px',
+          borderBottom: 'solid 1px gray',
+          paddingBottom: '20px'
+        }}>
+        <em>{props.tags}</em>
+    </li>
     </div>
   );
 };
